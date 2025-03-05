848fa1f445c923cbc3bc1bee502a9096a376125b
Luis Eduardo <luixst1@gmail.com>
2025-03-05 17:32:34 -0300

    [feature] | Iniciando os microservicos

---
+++ ms-event-manager/src/test/java/com/compass/ms_event_manager/MsEventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.ms_event_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEventManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
