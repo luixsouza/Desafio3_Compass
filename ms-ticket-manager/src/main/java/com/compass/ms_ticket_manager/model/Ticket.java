194a4ba757c436491c5e73888ac8deab40daffdf
Luis Eduardo <luixst1@gmail.com>
2025-03-12 15:50:11 -0300

    [feature] | Implementacao da camada de testes de Tickets (Controller e Service)

--- ms-ticket-manager/src/main/java/com/compass/ms_ticket_manager/model/Ticket.java
+++ ms-ticket-manager/src/main/java/com/compass/ms_ticket_manager/model/Ticket.java
@@ -1,13 +1,18 @@
 package com.compass.ms_ticket_manager.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Document(collection = "tickets")
 public class Ticket {
